18100a505d09d5ab3153b6d5b1d14cc5bb9b7498
Joshua <e0544333@u.nus.edu>
2023-02-24 21:05:47 +0800

    Create initial indexing logic
    - Added tokenization and stemming for each sentence in the document
    - Write to a postings.txt and dictionary.txt
    - How to interpret the file:
    -> The dictionary.txt contains all the unique terms and its location in the postings.txt
    -> the postings.txt contains the term and a list of all the file names that is split by the delimiter ':'

--- index.py
+++ index.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python3
+
+
+
+"""
+Different commands depending on environment
+Debug phase:
+python3 index.py -i ./sample_training_folder -d dictionary.txt -p postings.txt
+
+Build phase:
+Note: need to find directory to where the reuters data set is installed on your local machine
+
+Jun Wei:
+
+
+Joshua (MacOS):
+python3 index.py -i /Users/joshuayap/nltk_data/corpora/reuters/training -d dictionary.txt -p postings.txt
+"""
+
 import re
-import nltk
+import os
 import sys
 import getopt
+
+import nltk
+from nltk.corpus import PlaintextCorpusReader
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem.porter import PorterStemmer
+
+import json
+import pickle
+
+stemmer = PorterStemmer()
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
@@ -13,8 +41,40 @@ def build_index(in_dir, out_dict, out_postings):
     then output the dictionary file and postings file
     """
     print('indexing...')
-    # This is an empty method
-    # Pls implement your code in below
+    
+    corpus = PlaintextCorpusReader(in_dir, '.*')
+
+    index = {}
+
+    for doc_id in corpus.fileids():
+        doc = corpus.raw(doc_id)
+        # Get setences in each document.
+        sentences = sent_tokenize(doc)
+        
+        for sentence in sentences:
+            # Tokenize the sentence into words.
+            words = word_tokenize(sentence)
+
+            for word in words:
+                stemmed_word = stemmer.stem(word.lower())
+                # Add the stemmed token to the index
+                if stemmed_word not in index:
+                    index[stemmed_word] = []
+                if doc_id not in index[stemmed_word]:
+                    index[stemmed_word].append(doc_id)
+    
+    # Write the index to a file called dictionary.txt
+    with open(out_dict, 'w') as f:
+        for i, (key, value) in enumerate(index.items(), 1):
+            f.write(f"{key}:{i}\n")
+
+    # Create a posting list file called postings.txt
+    with open(out_postings, 'w') as f:
+        # Loop over the index and write each posting list to the file
+        for term, posting_list in index.items():
+            posting_list_str = ','.join(os.path.splitext(posting)[0] for posting in posting_list)
+            f.write(f"{term}:{posting_list_str}\n")
+
 
 input_directory = output_file_dictionary = output_file_postings = None
 
