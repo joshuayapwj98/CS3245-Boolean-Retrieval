5c290d28ddef4fe4f92255901f90a4093324a58a
junwei-tan <junweitan@u.nus.edu>
2023-02-28 21:51:25 +0800

    Add draft of search feature

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 
-
 """
 Different commands depending on environment
 Debug phase:
@@ -10,8 +9,8 @@ python3 index.py -i ./sample_training_folder -d dictionary.txt -p postings.txt
 Build phase:
 Note: need to find directory to where the reuters data set is installed on your local machine
 
-Jun Wei:
-
+Jun Wei (MacOS):
+python3 index.py -i /Users/tanju/nltk_data/corpora/reuters/training -d dictionary.txt -p postings.txt
 
 Joshua (MacOS):
 python3 index.py -i /Users/joshuayap/nltk_data/corpora/reuters/training -d dictionary.txt -p postings.txt
@@ -46,6 +45,7 @@ block_no = 0
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
 
+
 def build_index(in_dir, out_dict, out_postings):
     """
     build index from documents stored in the input directory,
@@ -71,7 +71,7 @@ def build_index(in_dir, out_dict, out_postings):
             index = {}
             if i != len(file_ids) - 1:
                 block_no += 1
-        
+
     final_block_no = merge_blocks_on_disk(0, block_no)
     write_dictionary_postings_to_disk(final_block_no, out_dict, out_postings)
 
@@ -96,11 +96,12 @@ def write_block_to_disk(index, block_no):
         output_line = json.dumps(output_dict)
         f.write(output_line)
 
+
 def merge_blocks_on_disk(start, end):
     if end - start >= 1:
         mid = (start + end) // 2
         left = merge_blocks_on_disk(start, mid)
-        right = merge_blocks_on_disk(mid+1, end)
+        right = merge_blocks_on_disk(mid + 1, end)
         merged_block_no = merge(left, right)
         return merged_block_no
     else:
@@ -117,7 +118,7 @@ def merge(left, right):
     with open(INTERMEDIATE_BLOCK + f'/{left}', 'r') as file1, open(INTERMEDIATE_BLOCK + f'/{right}', 'r') as file2:
         left_dictionary = json.load(file1)
         right_dictionary = json.load(file2)
-    
+
     for term, prop in right_dictionary.items():
         if term in left_dictionary:
             # if a term in right_dictionary can be found in left_dictionary, 
@@ -129,15 +130,15 @@ def merge(left, right):
         else:
             # if a term in right_dictionary cannot be found in left_dictionary, add the term to merged_dictionary
             merged_dictionary[term] = prop
-    
+
     for term, prop in left_dictionary.items():
         if term not in merged_dictionary:
             # if a term in left_dictionary cannot be found in merged_dictionary, add the term into to merged_dictionary
             merged_dictionary[term] = prop
-    
+
     block_no += 1
 
-     # write the merged dictionary to a new block on disk
+    # write the merged dictionary to a new block on disk
     with open(INTERMEDIATE_BLOCK + f'/{block_no}', 'w') as f:
         output_line = json.dumps(merged_dictionary)
         f.write(output_line)
@@ -147,12 +148,14 @@ def merge(left, right):
 
     # return the index of the new merged block
     return block_no
-    
+
+
 def write_dictionary_postings_to_disk(number, out_dict, out_postings):
     merged_dictionary = {}
 
     block_file_dir = INTERMEDIATE_BLOCK + f'/{number}'
-    with open(block_file_dir, 'r') as block_file, open(f'{out_dict}', 'w') as dictionary_file, open(f'{out_postings}', 'w') as postings_file:
+    with open(block_file_dir, 'r') as block_file, open(f'{out_dict}', 'w') as dictionary_file, open(f'{out_postings}',
+                                                                                                    'w') as postings_file:
         merged_dictionary = json.load(block_file)
         line_no = 1
         for term, postings_list in merged_dictionary.items():
@@ -165,13 +168,14 @@ def write_dictionary_postings_to_disk(number, out_dict, out_postings):
                 new_postings_list.append([postings_list[i]])
                 if skip_pointers_no != 0:
                     if i == 0 or i == next_index:
-                        next_index = i+ skip_distance - 1
+                        next_index = i + skip_distance - 1
                         new_postings_list[i].append(postings_list[next_index])
                         skip_pointers_no -= 1
 
             postings_file.write(term + " " + str(new_postings_list) + "\n")
             dictionary_file.write(term + " " + str(len(new_postings_list)) + " " + str(line_no) + "\n")
             line_no += 1
+
 
 input_directory = output_file_dictionary = output_file_postings = None
 
@@ -182,11 +186,11 @@ except getopt.GetoptError:
     sys.exit(2)
 
 for o, a in opts:
-    if o == '-i': # input directory
+    if o == '-i':  # input directory
         input_directory = a
-    elif o == '-d': # dictionary file
+    elif o == '-d':  # dictionary file
         output_file_dictionary = a
-    elif o == '-p': # postings file
+    elif o == '-p':  # postings file
         output_file_postings = a
     else:
         assert False, "unhandled option"
--- search.py
+++ search.py
@@ -3,9 +3,12 @@ import re
 import nltk
 import sys
 import getopt
+from queue import PriorityQueue
+
 
 def usage():
     print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
+
 
 def run_search(dict_file, postings_file, queries_file, results_file):
     """
@@ -16,14 +19,7 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     # This is an empty method
     # Pls implement your code in below
 
-
     # Open the dictionary file and postings file
-
-    # Basic operators ( () -> NOT -> AND -> OR )
-    # A AND B
-    # A OR B
-    # A OR NOT B
-    # A AND NOT B
 
     # logic to rearrange order of operations,
     # https://en.wikipedia.org/wiki/Binary_expression_tree
@@ -32,6 +28,98 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     # operators not in upper case
 
 
+def process_query(query):
+    # tokenize the query
+    split_query = query.split()
+    split_query_parenthesis_processed = []
+
+    i = 0
+    open_parenthesis_index = 0
+    close_parenthesis_index = 0
+    while i < len(split_query):
+        if split_query[i] == "(":
+            open_parenthesis_index = i
+            i += 1
+            continue
+        if split_query[i] == ")":
+            parenthesis_expression = split_query[open_parenthesis_index:i + 1]
+            processed_expression = process_parenthesis(parenthesis_expression)
+            split_query_parenthesis_processed.append(processed_expression)
+            i += 1
+            continue
+        if split_query[i] == "NOT":
+            split_query_parenthesis_processed.append(split_query[i:i + 2])
+            i += 2
+            continue
+        split_query_parenthesis_processed.append(split_query[i])
+        i += 1
+
+
+def process_parenthesis(parenthesis_expression):
+    return process_query_no_parenthesis(parenthesis_expression)
+
+
+def process_or_operator(operands):
+    temp_results = []
+
+    # TODO : OR merging
+    # for operand in args:
+    # or merging
+
+    return temp_results
+
+
+def process_and_operator(operands):
+    pq = PriorityQueue(len(args))
+    temp_results = []
+
+    for operand in operands:
+        if isinstance(operand, list):
+            pq.put((0, operand))
+            continue
+
+        # TODO: get the doc_frequency from the dictionary
+        doc_frequency = 99
+        pq.put(1 / dict.get(), operand)
+
+    while not pq.empty():
+        curr = pq.get()
+        # TODO: get postings list from postings file and merge
+
+
+def process_query_no_parenthesis(query_list):
+    temp_results = []
+
+    i = 0
+    while i < len(query_list):
+        if query_list[i] == "AND":
+            AND_operands = query_list[i - 1]
+            AND_operands.append(query_list[i + 1])
+            k = 1
+            while i + 2 * k < len(query_list) and query_list[i + 2 * k] == "AND":
+                AND_operands.append(query_list[i + k])
+                k += 1
+            temp_results.append(process_and_operator(AND_operands))
+            i = i + 2 * k
+            continue
+
+        temp_results.append(query_list[i])
+        i += 1
+
+    OR_operands = [i for i in temp_results if i != "OR"]
+    return process_or_operator(OR_operands)
+
+
+def get_postings_list(term, temp_dict, actual_positings):
+    if term[0] == "*":
+        return temp_dict.get(term[1:])
+
+    # get postings list from postings file
+    # remove punctuations
+    # remove stop words
+    # stem the words / lemmatize
+    # return the list of tokens
+    "bill OR Gates AND (vista OR XP) AND NOT mac"
 
 
 dictionary_file = postings_file = file_of_queries = output_file_of_results = None
@@ -44,7 +132,7 @@ except getopt.GetoptError:
 
 for o, a in opts:
     if o == '-d':
-        dictionary_file  = a
+        dictionary_file = a
     elif o == '-p':
         postings_file = a
     elif o == '-q':
@@ -54,7 +142,7 @@ for o, a in opts:
     else:
         assert False, "unhandled option"
 
-if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None :
+if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None:
     usage()
     sys.exit(2)
 
